041cebc5aa868292c4e46086c2e17817c4272fcf
prathamwho <praj79074@gmail.com>
2025-10-04 04:15:55 +0530

    Testing node-cron, cron, node-scheduler logic

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const {PORT} = require('./config/serverConfig');
-const { sendBasicEmail } = require('./services/email-service');
+const { PORT } = require('./config/serverConfig');
+const cron = require('node-cron');
 
-
+// const { sendBasicEmail } = require('./services/email-service');
 
 const setupAndStartServer = () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
 
-    app.listen(PORT, ()=>{
+    app.listen(PORT, () => {
         console.log(`Server started at port: ${PORT}`);
 
-        sendBasicEmail(
-            '"Support" <support@admin.com>',
-            'isaidwhoasked@gmail.com',
-            'This is a testing mail',
-            'Hey, How are you, I hope you like the support'
-        )
+        // sendBasicEmail(
+        //     '"Support" <support@admin.com>',
+        //     'isaidwhoasked@gmail.com',
+        //     'This is a testing mail',
+        //     'Hey, How are you, I hope you like the support'
+        // )
+
+        cron.schedule('*/2 * * * *', () => {
+            console.log('running a task every two minutes');
+        });
     });
 }
 
