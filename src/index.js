8cf2976416f0d5dcdefec284a0b1b4855043899d
prathamwho <praj79074@gmail.com>
2025-10-17 13:40:22 +0530

    Added Notification cron

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 const cron = require('node-cron');
 
+const TicketController = require('./controllers/ticket-controller');
+
 // const { sendBasicEmail } = require('./services/email-service');
 
 const setupAndStartServer = () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.post('/api/v1/tickets', TicketController.create);
 
     app.listen(PORT, () => {
         console.log(`Server started at port: ${PORT}`);
--- src/services/email-service.js
+++ src/services/email-service.js
@@ -1,4 +1,5 @@
 const sender = require('../config/emailConfig');
+const TicketRepository = require('../repository/ticket-repository');
 
 const sendBasicEmail = async (mailFrom, mailTo, mailSubject, mailBody) => {
     try {
@@ -14,6 +15,27 @@ const sendBasicEmail = async (mailFrom, mailTo, mailSubject, mailBody) => {
     }
 }
 
+const fetchPendingEmails = async (timestamp) => {
+    try {
+        const response = await repo.get({ status: "PENDING" });
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const createNotification = async (data) => {
+    try {
+        console.log(data);
+        const response = await repo.create(data);
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 module.exports = {
-    sendBasicEmail
+    sendBasicEmail,
+    fetchPendingEmails,
+    createNotification
 }
