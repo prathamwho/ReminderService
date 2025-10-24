0ee284aebd43d106722a6e29b9536bce305cecac
prathamwho <praj79074@gmail.com>
2025-10-24 11:31:42 +0530

    Subscribed to the Booking Service Message Queues

--- src/utils/messageQueue.js
+++ src/utils/messageQueue.js
@@ -1,41 +1,37 @@
 const amqplib = require('amqplib');
-const { MESSAGE_BROKER_URL, EXCHANGE_NAME} = require('../config/serverConfig');
-
+const { MESSAGE_BROKER_URL, EXCHANGE_NAME } = require('../config/serverConfig');
 const createChannel = async () => {
     try {
         const connection = await amqplib.connect(MESSAGE_BROKER_URL);
-        const channel = await connection.createChannel(); 
-        await channel.assertExchange(EXCHANGE_NAME, 'direct', false);
+        const channel = await connection.createChannel();
+        await channel.assertExchange(EXCHANGE_NAME, 'direct', { durable: false });
         return channel;
     } catch (error) {
         throw error;
     }
 }
 
-const subscribeMessage = async (channel, service,  binding_key) => {
+const subscribeMessage = async (channel, service, binding_key) => {
     try {
-        const applicationQueue = await channel.assertQueue('REMINDER_QUEUE');
+        const applicartionQueue = await channel.assertQueue('REMINDER_QUEUE');
+        channel.bindQueue(applicartionQueue.queue, EXCHANGE_NAME, binding_key);
 
-        channel.bindQueue(applicationQueue.queue, EXCHANGE_NAME, binding_key);
-
-        channel.consume(applicationQueue.queue, msg => {
+        channel.consume(applicartionQueue.queue, msg => {
             console.log('received data');
             console.log(msg.content.toString());
-            const payload = JSON.parse(msg.content.toString());
-            service(payload);
             channel.ack(msg);
         });
     } catch (error) {
         throw error;
     }
-    
 }
 
 const publishMessage = async (channel, binding_key, message) => {
     try {
         await channel.assertQueue('REMINDER_QUEUE');
         await channel.publish(EXCHANGE_NAME, binding_key, Buffer.from(message));
-    } catch (error) {
+    }
+    catch (error) {
         throw error;
     }
 }
